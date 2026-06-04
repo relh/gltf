5069dc231b11ffc169da78c757e761dbad5f14af
treeform <starplant@gmail.com>
2026-06-03 18:10:37 -0700

    Add Draco Tests

---
+++ tests/dracos/tests.nim
@@ -0,0 +1,14 @@
+import
+  test_bitstreams,
+  test_entropy,
+  test_meshes,
+  test_octs,
+  test_samples
+
+runBitstreamTests()
+runEntropyTests()
+runMeshTests()
+runOctTests()
+runSampleTests()
+
+echo "Draco tests passed"
